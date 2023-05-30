0a3e4802cca8fc82e242c5bfc90a12554296c0d6
Kaykjosea <129893557+Kaykjosea@users.noreply.github.com>
2023-05-30 16:16:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function notas(){
+    let nome = document.getElementById('nome').value
+    let n1 = parseInt(document.getElementById('n1').value)
+    let n2 = parseInt(document.getElementById('n2').value)
+    let n3 = parseInt(document.getElementById('n3').value)
+    
+    media = (n1+n2+n3)/3
+
+    if(media >= 6){
+    document.getElementById('valor').innerHTML = `${nome}, sua média foi ${+ media}, você foi aprovado!`
+    }
+   else{
+   document.getElementById('valor').innerHTML = `${nome}, sua média foi ${+ media}, você foi reprovado!` 
+   }
+
+}
